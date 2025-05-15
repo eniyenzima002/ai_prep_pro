783b73e6063979b5ca9e9dcee7b17bc4158ab98b
Your Name <you@example.com>
2025-05-15 00:34:57 -0500

    update repo

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { Toaster } from "sonner";
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AI Preps",
+  description: "An AI_powered call for anything important to do with friends.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiased pattern`}
+      >
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
